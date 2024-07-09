c43526566d2c318bd76ef44a33bf5d6065d3ee14
Preeti IT <preeti@HQ.BSPTCL.CO.IN>
2024-07-09 16:46:47 +0530

    Initial commit

---
+++ components/DisplayMapInfo.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const DisplayMapInfo = () => {
+  return <div></div>;
+};
+
+export default DisplayMapInfo;
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import DisplayMap from '../components/DisplayMap';
+import DisplayMapInfo from '../components/DisplayMapInfo';
+
+
+function App() {
+  return (
+    <>  
+      <DisplayMap />
+      <DisplayMapInfo />
+    </>
+  );
+}
+
+export default App;
